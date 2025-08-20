5b3037eabcbe7044b6b5257b2d94943cb223485b
Joe <sdauwangzh@163.com>
2025-08-20 09:40:29 +0800

    [fix]去掉请求头的限制 && 默认使用esm

--- src/index.js
+++ src/index.js
@@ -9,22 +9,6 @@ class V2exParser {
     constructor(options = {}) {
         // 设置默认域名
         this.baseUrl = options.baseUrl || 'https://v2ex.com';
-
-        this.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36';
-        this.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': this.userAgent
-        };
     }
 
     /**
